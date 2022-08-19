0e79e91bb52b029d0f4d858d71e27338eb6b201f
yjkwon07 <dudwns9073@nate.com>
2022-08-19 19:44:23 +0900

    [theme]: breakpoints  extend 설정 - storybook 추가

---
+++ src/theme/foundations/breakpoints.ts
@@ -0,0 +1,11 @@
+const breakpoints = {
+  base: '0px',
+  xs: '480px',
+  sm: '768px',
+  md: '896px',
+  lg: '1200px',
+  xl: '1920px',
+  '2xl': '2100px',
+};
+
+export default breakpoints;
--- src/theme/foundations/sizes.ts
+++ src/theme/foundations/sizes.ts
@@ -1,18 +1,6 @@
-const breakPoints = ['base', 'xs', 'sm', 'md', 'lg', 'xl', '2xl'] as const;
-type BreakPoints = Record<typeof breakPoints[number], string>;
-export type BreakPointsObject = Partial<BreakPoints>;
+import breakpoints from './breakpoints';
 
-export const breakpoints: BreakPointsObject = {
-  base: '',
-  xs: '480px',
-  sm: '768px',
-  md: '896px',
-  lg: '1200px',
-  xl: '1920px',
-  '2xl': '2100',
-};
-
-export const sizes: { container: BreakPointsObject } = {
+export const sizes = {
   container: {
     sm: breakpoints.sm,
     md: breakpoints.md,

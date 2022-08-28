3e01095cd31ca7895929750e1fd519b1af0d47ed
yjkwon07 <dudwns9073@nate.com>
2022-08-29 01:29:26 +0900

    [component]: Breakpoints Default, Adjust 분리

--- src/components/Breakpoints/index.stories.tsx
+++ src/components/Breakpoints/index.stories.tsx
@@ -1,10 +1,33 @@
-import { Alert, AlertIcon, Box, Flex, Text, useBreakpoint, useTheme } from '@chakra-ui/react';
+import { Alert, AlertIcon, Box, Flex, Text, ThemeTypings, useBreakpoint, useTheme } from '@chakra-ui/react';
 
 export default {
   title: 'Theme/Breakpoints',
 };
 
 export const Default = () => {
+  const theme = useTheme();
+  const breakpointsProps = theme.breakpoints;
+  const BREAKPOINT_LIST = Object.keys(breakpointsProps).sort(
+    (a, b) => Number(breakpointsProps[a].replace(/(px)/, '')) - Number(breakpointsProps[b].replace(/(px)/, '')),
+  ) as ThemeTypings['breakpoints'][];
+
+  return (
+    <Flex direction="column" rowGap="5" p={4}>
+      {BREAKPOINT_LIST.map((breakpoint) => (
+        <Box
+          key={breakpoint}
+          w={breakpointsProps[breakpoint] !== '0px' ? breakpointsProps[breakpoint] : 'full'}
+          p={5}
+          bg={breakpointsProps[breakpoint] !== '0px' ? 'green.300' : 'white'}
+        >
+          <Text fontSize="2xl">{`${breakpoint} ${breakpointsProps[breakpoint]}`}</Text>
+        </Box>
+      ))}
+    </Flex>
+  );
+};
+
+export const Adjust = () => {
   const bp = useBreakpoint();
 
   const theme = useTheme();

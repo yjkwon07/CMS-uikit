6a18394734035206852531161d5fd28cde861f86
yjkwon07 <dudwns9073@nate.com>
2022-08-29 18:30:21 +0900

    [theme]: SimpleGrid extend 설정 - storybook 추가

---
+++ src/theme/components/SimpleGrid.ts
@@ -0,0 +1,10 @@
+import { ComponentStyleConfig } from '@chakra-ui/react';
+
+const SimpleGrid: ComponentStyleConfig = {
+  baseStyle: {},
+  sizes: {},
+  variants: {},
+  defaultProps: {},
+};
+
+export default SimpleGrid;
